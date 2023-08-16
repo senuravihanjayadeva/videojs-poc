7a419a0b81b6ac70d8b2ac9ecd45486b9cdf2cb2
senuravihanjayadeva <senurajayadeva@gmail.com>
2023-08-16 19:28:56 +0530

    Initial Implementation for VideoJS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SampleVideo from "./sample.mp4"
+// This imports the functional component from the previous sample.
+import VideoJS from './components/VideoJS'
 
-function App() {
+const App = () => {
+  const playerRef = React.useRef(null);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [{
+      src: SampleVideo,
+      type: 'video/mp4'
+    }]
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on('waiting', () => {
+      // videojs.log('player is waiting');
+    });
+
+    player.on('dispose', () => {
+      // videojs.log('player will dispose');
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Rest of app here</div>
+      <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+      <div>Rest of app here</div>
+    </>
   );
 }
 
